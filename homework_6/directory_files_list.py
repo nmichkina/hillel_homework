4106cdba9d3684772a3d43a4459258ffa422a523
nmichkina <natalymitch@gmail.com>
2023-09-11 18:12:09 +0300

    files list task first steps

---
+++ homework_6/directory_files_list.py
@@ -0,0 +1,25 @@
+"""
+Написати програму, яка просканує кореневу папку вашого проєкту, збереже у файл files_size.txt назви та розмір файлів,
+і надрукує назву найбільшого файлу та його розмір.
+"""
+
+import os
+
+path = r"C:\Users\emgso\PycharmProjects\hillel_homework"
+file = "files_size.txt"
+
+fun = lambda x: os.path.isfile(os.path.join(path, x))
+files_list = filter(fun, os.listdir(path))
+
+size_of_file = [
+    (f, os.stat(os.path.join(path, f)).st_size)
+    for f in files_list
+]
+
+with open(os.path.join(path, file), 'w') as fp:
+
+    #this fp.write("New file created")
+pass
+
+for f, s in size_of_file:
+    print("{} : {}MB".format(f, round(s / (1024 * 1024), 3)))
