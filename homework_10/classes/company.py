747e2c8712ba8decca3fe4826577c626590b3afd
nmichkina <natalymitch@gmail.com>
2023-10-02 18:06:57 +0300

    classes employee and company reworked

--- homework_10/classes/company.py
+++ homework_10/classes/company.py
@@ -1,6 +1,18 @@
-class Company:
+"""
+Create a class describing any company. For example, Toshiba or Apple
+"""
 
-    def __init__(self, name: str, company_type: str):  # attr of instance
-        self.company_type = company_type
-        self.name = name
-        self.address = []
+
+class Company:
+    company = 'Toshiba'  # attribute of class
+
+    def __init__(self, companytype: str, address: str):  # attribute of instance
+        self.companytype = companytype
+        self.address = address
+
+    def show(self):
+        print("Company name is: " + Company.company + " and company type is: " + self.companytype)
+
+
+obj = Company("Product", "Haifa")
+obj.show()
--- homework_10/classes/employee.py
+++ homework_10/classes/employee.py
@@ -1,13 +1,22 @@
-import company
+"""
+Create a class with a description of the worker. Any worker or employees.
+"""
 
 
 class Employee:
-    company = company.name  # attr of class
+    company = "Toshiba"  # attribute of class
 
-    def __init__(self, name: str, age: int, salary: int):  # attr of instance
+    def __init__(self, name: str, age: int, salary: int):  # attribute of instance
         self.name = name
         self.age = age
         self.salary = salary
         self.company = Employee.company
         self.address = []
         self.bonus = None
+
+    def showname(self):
+        print("Employee name is: " + self.name)
+
+
+obj = Employee("John", 18, 500)
+obj.showname()
